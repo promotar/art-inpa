a11af6854d2f783bf090842da75d65b58d86b84a
Ziad Mansor <promotar.info@gmail.com>
2026-07-28 01:38:32 +0300

    pug page bulder fix

--- modules/page-builder/src/PageBuilderServiceProvider.php
+++ modules/page-builder/src/PageBuilderServiceProvider.php
@@ -9,6 +9,8 @@ class PageBuilderServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
+        $this->loadMigrationsFrom(dirname(__DIR__).'/database/migrations');
+
         View::addNamespace('page-builder', dirname(__DIR__).'/resources/views');
     }
 }
